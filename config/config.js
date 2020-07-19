e3460e2c555276bb2a4ecb0f48b75f762dead013
dahlia15 <dahliaamade1@gmail.com>
2020-07-19 12:58:43 -0400

    packagejson

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Welcome7!",
+    "password": "root",
     "database": "GP3",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
